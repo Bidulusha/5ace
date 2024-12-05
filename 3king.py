0bd27aef8776be7a7b1a490f3d740ada7483fb26
Bidulusha <74060730+Bidulusha@users.noreply.github.com>
2024-12-05 21:28:51 +0300

    scvadnoy petuh

--- 3king.py
+++ 3king.py
@@ -1,6 +1,9 @@
 import csv
 from decimal import Decimal
 import matplotlib.pyplot as plt
+from scipy.spatial.transform import Rotation
+from mpl_toolkits.mplot3d import Axes3D
+import pandas as pd
 
 mas = []
 
@@ -15,13 +18,39 @@ with open('logs/beacon_human.csv', newline='') as csvfile:
             mas.append(l)
 
 
-raz = []
+'''mm = []
+for i in mas:
+    quaternion = (i[1],i[2],i[3],i[4])  
+    rot = Rotation.from_quat(quaternion)
+    rot_euler = rot.as_euler('xyz', degrees=True)
+    rr = list(rot_euler)
+    mm.append(rr)'''
 
-for i in range(1, len(mas)):
-    raz.append((i, mas[i][0] - mas[i - 1][0]))
+x = []
+y = []
+z = []
 
-for i in raz:
-    print(*i)
+
+fig = plt.figure()
+ax = fig.add_subplot(projection='3d')
+plt.plot([0, mm[0][0]], [0, mm[0][1]], [0, mm[0][2]], color = 'red')
+plt.plot([0,float(mas[0][6]) * 100 ], [0,float(mas[0][7])* 100], [0,float(mas[0][8])* 100]) # 6 7 8
+plt.show()
+
+
+'''timemas = [31557600000000, 86400000000, 3600000000, 60000000, 1000000]
+datemas = []
+
+for i in mas:
+    t = int(i[0])
+    date = []
+    for j in timemas:
+        date.append(t // j)
+        t %= j
+    datemas.append(date)
+
+print(datemas)'''
+
 '''x = [] #9
 y = [] #10 
 

4d8a8b980fec09a18f46ea14c06994fd406a511d
Bidulusha <74060730+Bidulusha@users.noreply.github.com>
2024-12-06 07:45:09 +0300

    add some visual

--- king3.py
+++ king3.py
@@ -1,9 +1,11 @@
-from ace4 import get_rotation_matrix
 import newmath
 import math
 from math import pi as PI
+import os
 import csv
 
+vert = 62.2
+gor = 48.8
 
 def quat_to_eul_mas(m):
     mas = []
@@ -11,15 +13,22 @@ def quat_to_eul_mas(m):
         mas.append((newmath.eci_ro_euler(i[5],i[6],i[7],i[8])))
     return mas
 
-vert = 62.2
-gor = 48.8
+
+##################################GET FotoTime##################################
+dirs = os.listdir('pic2')
+
+f = -1
+for i in dirs:
+    c = i.find('.jpg')
+    if c != -1:
+        fototime = i[17:19]
+        break
+##################################GET FotoTime##################################
 
 
-#matrix = get_rotation_matrix()
-
+##################################GET MAS##################################
 mas = []
-
-with open('logs/beacon_human.csv', newline='') as csvfile:
+with open('pic2/logs/beacon_human.csv', newline='') as csvfile:
     spamreader = csv.reader(csvfile, delimiter=',', quotechar='|')
     for row in spamreader:
         if row != [] and row[0] != 'time usec':
@@ -31,17 +40,21 @@ with open('logs/beacon_human.csv', newline='') as csvfile:
 
 h = newmath.find_height(mas)
 euler_mas = quat_to_eul_mas(mas)
-
 for i in euler_mas:
     print(i)
+##################################GET MAS##################################
 
-'''
-for i in matrix:
-    print(i)
+##################################GET time from FotoTime##################################
+datetime = newmath.to_time(mas)
+tt = []
+for i in range(1,len(datetime)):
+    if datetime[i] > fototime and datetime[i-1] < fototime or datetime[i] == fototime or datetime[i + 1] > fototime and datetime[i] < fototime:
+        tt.append(datetime[i])
 
-'''
+print(tt)
+##################################GET time from FotoTime##################################
 
-
+##################################GET new latitude and longtitude##################################
 for i in euler_mas:
     bigv = math.tan(PI * (i[0] + vert / 2) / 180) * h / 1000
     smalv = math.tan(PI * (i[0] - vert / 2) / 180) * h / 1000
@@ -54,5 +67,4 @@ grad = 360 / 40000
 
 l = [mas[0][9] + bigv * grad ,mas[0][9] + smalv * grad]
 f = [mas[0][10] + bigg * grad, mas[0][10] + smalg * grad]
-
-#print(a)
+##################################GET new latitude and longtitude##################################
--- newmath.py
+++ newmath.py
@@ -11,3 +11,16 @@ def eci_ro_euler(q1, q2, q3, q4):
     b = math.asin (2*(q1*q3-q4*q2))
     c = math.atan2 (2*(q1*q2+q3*q4),1-2*(q2*q2+q3*q3))
     return a,b,c
+
+def to_time(mas):
+    timemas = [31557600000000, 86400000000, 3600000000, 60000000, 1000000]
+    datemas = []
+
+    for i in mas:
+        t = int(i[0])
+        date = []
+        for j in timemas:
+            date.append(t // j)
+            t %= j
+        datemas.append(date)
+    return datemas
